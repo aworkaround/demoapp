c6e4ab6e30d03303f1fce6b3bff17e39f5e6d792
Kamal Kumar <kamal2k89@gmail.com>
2022-05-28 22:36:19 +0530

    initial commit

---
+++ run.py
@@ -0,0 +1,26 @@
+from flask import Flask, make_response, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.get('/')
+def index():
+    username = request.cookies.get('username')
+    return render_template('index.html', username=username)
+
+@app.post('/login')
+def login():
+    username = request.form['username']
+    resp = make_response(render_template('index.html', username=username.title(), login=True))
+    resp.set_cookie('username', username.title())
+    return resp
+
+@app.get('/logout')
+def logout():
+    resp = make_response(render_template('index.html', logout=True))
+    resp.set_cookie('username', '', expires=0)
+    return resp
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080, host='0.0.0.0')
